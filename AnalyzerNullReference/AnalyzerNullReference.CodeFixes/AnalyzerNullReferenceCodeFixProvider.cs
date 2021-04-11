87cdb8263ce9522c959008809b4fd32eeecc247b
ahmat <ahmatan10@gmail.com>
2021-04-11 21:17:15 +0300

    Добавьте файлы проекта.

---
+++ AnalyzerNullReference/AnalyzerNullReference.CodeFixes/AnalyzerNullReferenceCodeFixProvider.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AnalyzerNullReference
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AnalyzerNullReferenceCodeFixProvider)), Shared]
+    public class AnalyzerNullReferenceCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(AnalyzerNullReferenceAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+
+            var diagnostic = context.Diagnostics.First();
+
+            var document = context.Document;
+
+            var root = await document.GetSyntaxRootAsync();
+
+            var declaration = root.FindNode(diagnostic.Location.SourceSpan);
+
+            context.RegisterCodeFix(CodeAction.Create("Delete null reference check", async c =>
+            {
+                var statement = root.FindNode(diagnostic.Location.SourceSpan);
+                while ((statement.Parent.Kind() != SyntaxKind.IfStatement) && //delete if node
+                       (statement.Parent.Kind() != SyntaxKind.LocalDeclarationStatement) && //delete variable node
+                       (statement.Parent.Kind() != SyntaxKind.ReturnStatement)) //delete return node
+                {
+                    statement = statement.Parent;
+                }
+
+                var newRoot = root.RemoveNode(statement.Parent, SyntaxRemoveOptions.KeepNoTrivia);
+
+                return document.WithSyntaxRoot(newRoot);
+
+            }), diagnostic);
+
+        }
+
+    }
+}
---
+++ AnalyzerNullReference/AnalyzerNullReference/AnalyzerNullReferenceAnalyzer.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+
+namespace AnalyzerNullReference
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AnalyzerNullReferenceAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "AnalyzerNullReference";
+
+
+        private static readonly string Title = "Remove variable checks for null refeence";
+        private static readonly string MessageFormat = "Waiting deletion if with check null reference";
+        private static readonly string Description = "Remove null reference checking";
+        private const string Category = "Usage";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+        public override void Initialize(AnalysisContext context)
+        {
+
+            context.RegisterSyntaxTreeAction(syntaxTreeContext =>
+            {
+                var root = syntaxTreeContext.Tree.GetRoot(syntaxTreeContext.CancellationToken);
+
+                foreach (var statement in root.DescendantNodes().OfType<LiteralExpressionSyntax>())
+                {
+                    if (statement.Kind() != SyntaxKind.NullLiteralExpression)
+                    {
+                        continue;
+                    }
+
+                    if (!(statement.Parent.Kind() is SyntaxKind.EqualsExpression))
+                    {
+                        continue;
+                    }
+
+                    if ((statement.Parent.Parent.Kind() is SyntaxKind.IfStatement) ||  //for check in simple if
+                        (statement.Parent.Parent.Kind() is SyntaxKind.EqualsValueClause) || //for check in Equals bool variable
+                        (statement.Parent.Parent.Kind() is SyntaxKind.ConditionalExpression) || //for check in ternary if
+                        (statement.Parent.Parent.Kind() is SyntaxKind.ReturnStatement)) //for check in return
+                    {
+                        var diagnostic = Diagnostic.Create(Rule, statement.Parent.GetLocation());
+                        syntaxTreeContext.ReportDiagnostic(diagnostic);
+                    }
+                }
+            });
+
+        }
+    }
+}
+
